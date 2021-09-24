f1da6290901e15a0f846c8b03fbaa6d9edfb9e6b
schott-anderson <schott.anderson@hotmail.com>
2021-09-24 13:46:09 -0300

    Add home and bookmarks module and components and sets ups your respective router

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'd4a609c66cd0e79f29b1420a4a9ab3ae',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'd4a609c66cd0e79f29b1420a4a9ab3ae',
 };
 
 /*
